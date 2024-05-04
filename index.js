dcca16fd609690d2d14d7ca95b65a1914569874c
Richard Van Camp <richard.van.camp@wolt.com>
2024-05-05 00:02:22 +0900

    Feat: update to latest lib versions

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { TimelineMax, TweenLight } from 'gsap'
+import gsap from 'gsap'
 
 const BACKGROUND_IMG = './rick.jpg'
 
-window.onload = function() {
+window.onload = function () {
   return loadImg(BACKGROUND_IMG)
     .then(({ path }) => {
       const main = document.getElementById('main')
-      TweenMax.to(main, 0, { className: 'background' })
+      gsap.to(main, 0, { className: 'background' })
     })
     .finally(() => {
       animateInPersonData()
@@ -16,22 +16,22 @@ window.onload = function() {
 function animateInPersonData() {
   const codeChildren = [...document.querySelectorAll('.key-value-pair')]
   const brackets = document.querySelectorAll('.bracket')
-  const tl = new TimelineMax({ repeat: 0 })
+  const tl = gsap.timeline({ repeat: 0 })
   tl.staggerFromTo(codeChildren, 2, { opacity: 0 }, { opacity: 1 }, 0.1)
     .staggerTo(brackets, 0, {
       opacity: 1,
       color: 'white',
-      fontWeight: 'bold'
+      fontWeight: 'bold',
     })
     .staggerTo(brackets, 1, {
       opacity: 1,
       color: 'hsl(0, 0%, 55%)',
-      fontWeight: 'normal'
+      fontWeight: 'normal',
     })
 }
 
 function loadImg(path) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const img = new Image()
     img.onload = () => resolve({ path, status: 'ok' })
     img.onerror = () => resolve({ path, status: 'error' })
